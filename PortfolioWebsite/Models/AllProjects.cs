90ce6918dd52121580b0d2c6eac146ae5659ce4c
Sebastian Burman <sebastianjburman@gmail.com>
2022-06-09 23:02:59 -0400

    Added project cards to homepage

---
+++ PortfolioWebsite/Models/AllProjects.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioWebsite.Models
+{
+    public class AllProjects
+    {
+        public List<PortfolioProject> allProjects;
+        
+        public AllProjects()
+        {
+           allProjects = new List<PortfolioProject>();
+           allProjects.Add(new PortfolioProject("Calculator","idk", "A simple calculator built with react state.",
+               "https://sebastianreactcalculator.netlify.app/", "https://github.com/sebastianjburman/Simple-React-Calculator"));
+        }
+    }
+}
---
+++ PortfolioWebsite/Models/PortfolioProject.cs
@@ -0,0 +1,19 @@
+﻿namespace PortfolioWebsite.Models
+{
+    public class PortfolioProject
+    {
+        public string? Name { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Description { get; set; }
+        public string? LinkToProject { get; set; }
+        public string? RepoLink { get; set; }
+        public PortfolioProject(string? name, string? imagePath, string? description, string? linkToProject, string? repoLink)
+        {
+            Name = name;
+            ImagePath = imagePath;
+            Description = description;
+            LinkToProject = linkToProject;
+            RepoLink = repoLink;
+        }
+    }
+}
